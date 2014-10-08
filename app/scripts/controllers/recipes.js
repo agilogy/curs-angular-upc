4b6bcfb3cadcd32a83c2ef4778bf60549432df35
Sergio Gimeno <gimeno.sergio@gmail.com>
2014-10-08 02:08:34 +0200

    Desar receptes al servidor i llistar-les

--- app/scripts/controllers/recipes.js
+++ app/scripts/controllers/recipes.js
@@ -1,22 +1,33 @@
 'use strict';
 
 angular.module('cursAngularUpcApp.recipes', [
-	'ngRoute'
+	'ngRoute',
+	'cursAngularUpcApp.rest'
 ])
 
 .config(function($routeProvider) {
 
 	$routeProvider
-		.when('/recipes', {
-			templateUrl: 'views/recipes.html'
-		})
-		.when('/recipes/new', {
-			templateUrl: 'views/recipe-form.html',
-			controller: 'RecipeFormCtrl'
-		});
+	.when('/recipes', {
+		templateUrl: 'views/recipes.html',
+		controller: 'RecipesCtrl'
+	})
+	.when('/recipes/new', {
+		templateUrl: 'views/recipe-form.html',
+		controller: 'RecipeFormCtrl'
+	});
 })
 
-.controller('RecipeFormCtrl', function($scope) {
+.controller('RecipesCtrl', function($scope, RestService) {
+
+	RestService.getAll()
+		.then(function(response){
+			$scope.recipes = response.data;
+		});
+
+})
+
+.controller('RecipeFormCtrl', function($scope, $location, RestService) {
 
 
 	$scope.formData = {
@@ -32,6 +43,13 @@ angular.module('cursAngularUpcApp.recipes', [
 
 	$scope.send = function(){
 		console.log($scope.formData);
+		RestService.addRecipe($scope.formData)
+			.then(function(resp){
+				if (resp.status === 200)
+					$location.path('/recipes');
+			}, function(resp){
+				console.error(resp);
+			});
 	};
 
 });
---
+++ app/scripts/services/rest.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('cursAngularUpcApp.rest',[])
+
+.service('RestService', function($http){
+
+	var apiUrl = 'http://recipesapi.herokuapp.com:80/api';
+
+	this.getAll = function(){
+		return $http.get(apiUrl + '/recipes');
+	};
+
+	this.addRecipe = function(recipe){
+		return $http.post(apiUrl + '/recipes', recipe);
+	};
+})
+
+;
