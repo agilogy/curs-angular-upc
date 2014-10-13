6b344b3fc7de28a1fe1c757dd7db530943e4b248
Manel Perez <manel.perez@agilogy.com>
2014-10-13 10:56:24 +0200

    navbar files

---
+++ app/scripts/directives/navbar.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('cursAngularUpcApp.directives.navbar', [])
+.directive('myNavbar', function($location){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		templateUrl: '/scripts/directives/navbar.tmp.html',
+		replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			
+		}
+	};
+});
